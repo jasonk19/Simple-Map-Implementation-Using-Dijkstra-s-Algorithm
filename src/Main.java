8bdffd1266f335928a39e77afb70c7f3848c0d6d
jasonk19 <13520080@std.stei.itb.ac.id>
2022-05-31 23:59:51 +0700

    add user input

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,19 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter test file (*.txt): ");
+    String fileName = scanner.next();
+    System.out.print("Starting point: ");
+    Character sourceNode = scanner.next().charAt(0);
+    System.out.print("End point: ");
+    Character destinationNode = scanner.next().charAt(0);
+    scanner.close();
+
     ConnectionCollection collection = new ConnectionCollection();
     List<String> result = new ArrayList<>();
     FileReader fr = new FileReader();
-    fr.readFile("test1.txt", result);
+    fr.readFile(fileName, result);
 
     for (String data : result) {
       String[] splitData = data.split("\\s+");
@@ -16,8 +25,8 @@ public class Main {
     }
 
     List<Connection> connections = collection.getCollection();
-    Node source = new Node('A');
-    Node destination = new Node('Z');
+    Node source = new Node(Character.toUpperCase(sourceNode));
+    Node destination = new Node(Character.toUpperCase(destinationNode));
 
     Dijkstra solver = new Dijkstra(connections, source, destination);
     solver.solve();
